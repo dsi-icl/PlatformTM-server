0f9ae01dd0cf6160f3acb1332370dffd54154683
Ibrahim Emam <i.emam@imperial.ac.uk>
2014-09-16 13:58:12 +0100

    Testing with dataset service

---
+++ eTRIKS.Commons.DataAccess/DatasetRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTRIKS.Commons.Core.Domain.Model;
+using eTRIKS.Commons.Core.Domain.Model.Templates;
+
+namespace eTRIKS.Commons.DataAccess
+{
+    class DatasetRepository : GenericRepository<Dataset, string>
+    {
+       
+        
+        public DatasetRepository(DbContext dataContext) : base(dataContext)
+        {
+            DataContext = dataContext;
+        }
+
+        public DatasetRepository(IDbSet<Dataset> entities) : base(entities)
+        {
+            
+        }
+
+        /// <summary>
+        /// Returns dataset with variables resulting from merge between Variable_def and DomainVariable
+        /// Variables from Varaible_Def will have Variable_REf attributes, variables from domainVariables will not
+        /// 
+        /// Dataset{
+        /// "Domain":{name:..., class:...},
+        /// "Variables":[
+        ///     {"name":..., ...etc , ordernumber:...., required:..., sequence:...}
+        /// ]
+        /// } 
+        /// </summary>
+        //public void GetMergedDataset()
+        //{
+        //    DbSet doaminVariableSet = DataContext.Set<DomainTemplateVariable>();
+        //    Entities
+        //        .Include(d => d.Domain)
+        //        .Include(d => d.Variables
+        //            .Select(v => v.Variable))
+        //        .Join(
+        //          //  doaminVariableSet)
+        //         ;
+        //}
+    }
+}
--- eTRIKS.Commons.DataAccess/DomainRepository.cs
+++ eTRIKS.Commons.DataAccess/DomainRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using eTRIKS.Commons.Core.Domain.Interfaces;
@@ -9,11 +10,23 @@ using eTRIKS.Commons.Core.Domain.Model.Templates;
 
 namespace eTRIKS.Commons.DataAccess
 {
-    class DomainRepository : GenericRepository<DomainTemplate, string> , IRepository<DomainTemplate, string>
+    class DomainRepository : GenericRepository<DomainTemplate, string>
     {
         public DomainRepository(DbContext dataContext) : base(dataContext)
         {
             //TODO: Implement here methods for getDomain, getDomainWithVariables ??
+
+            
+        }
+
+        public void getDomainWithVariables()
+        {
+            
+        }
+
+        public void addVariableToDomain()
+        {
+            
         }
     }
 }
