4d1cac20c051239c57ba8e1de60bbb25e07aabd7
Dilshan Silva <d.silva10@imperial.ac.uk>
2014-12-16 11:57:32 +0000

    Updated Spec

---
+++ eTRIKS.Commons.Service/DTOs/VariableReferenceDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTRIKS.Commons.Service.DTOs
+{
+    public class VariableReferenceDTO
+    {
+        public string OrderNumber { get; set; }
+        public string IsRequired { get; set; }
+        public string KeySequence { get; set; }
+
+        public string VariableDefinition { get; set; }
+        public string VariableDefinitionId { get; set; }
+
+        public string Dataset { get; set; }
+        public string DatasetId { get; set; }
+    }
+}
