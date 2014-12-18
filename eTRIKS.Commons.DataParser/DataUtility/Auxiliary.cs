a8c5c20c0d13963ce5ca27ba22f31503b5c4f87d
Dilshan Silva <d.silva10@imperial.ac.uk>
2014-12-18 21:18:20 +0000

    Load Domain Template

---
+++ eTRIKS.Commons.DataParser/DataUtility/Auxiliary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTRIKS.Commons.DataParser.DataUtility
+{
+    class Auxiliary
+    {
+        public string processDictionaryIdForTemplateVariable(string input)
+        {
+            if (input.Contains('('))
+            {
+                if (!input.Contains(':')) //to filter out (Fsdtm-1-3:Classifier.RequiredVariable)
+                {
+                    char[] removeParanthesis = { '(', ')' };
+                    return "CL-" + input.Replace("\nISO 3166", "").Trim(removeParanthesis);
+                }
+            }
+            return null;
+        }
+
+        public string getIdOfCVTerm(string field)
+        {
+            if (field.Length < 1)
+                return null;
+            //IGenericDataService<CVterm_TBL> chkExist = new DataManagementService<CVterm_TBL>();
+
+            //CVterm_TBL Cvterm = chkExist.GetSingleOptimised(o => o.Name.Equals(field));
+
+            ////CVterm_TBL Cvterm = chkExist.GetSingle(o => o.Name.Equals(field));
+            //if (Cvterm != null)
+            //{
+            //    return Cvterm.OID.ToString();
+            //}
+            return "Record Not Found";
+        }
+    }
+}
