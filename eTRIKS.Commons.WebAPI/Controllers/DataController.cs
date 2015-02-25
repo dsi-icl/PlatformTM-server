1e3279b2105c3d6ffbeeb2c8564b2b5d40228d81
Ibrahim Emam <i.emam@imperial.ac.uk>
2015-02-25 13:45:12 +0000

    Adding DataController

---
+++ eTRIKS.Commons.WebAPI/Controllers/DataController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eTRIKS.Commons.WebAPI.Controllers
+{
+    //[RoutePrefix("api/studies")]
+    public class DataController : ApiController
+    {
+        //private ActivityService _activityService;
+
+        //public DataController(ActivityService activityService)
+        //{
+        //    _activityService = activityService;
+        //}
+
+        [HttpPost]
+        [Route("api/studies/{studyId}/data/clinical/observations")]
+        public string getObservations(string studyId, [FromBody] List<string> observations)
+        {
+            Console.Out.WriteLine(studyId);
+
+            Console.Out.WriteLine(observations);
+            return "OK";
+        }
+
+    }
+}
