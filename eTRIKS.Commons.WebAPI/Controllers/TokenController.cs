d7590b7c646e4389292219b1896a99610f777683
v.elyasigomari <v.elyasigomari@imperial.ac.uk>
2016-11-17 17:17:57 +0000

    Token Controller

---
+++ eTRIKS.Commons.WebAPI/Controllers/TokenController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+using eTRIKS.Commons.Service.Services.UserManagement;
+
+namespace eTRIKS.Commons.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        private readonly TokenAuthOptions tokenOptions;
+        UserAccountService _accountService { get; set; }
+
+        public TokenController(TokenAuthOptions tokenOptions)
+        {
+            this.tokenOptions = tokenOptions;
+            //this.bearerOptions = options.Value;
+            //this.signingCredentials = signingCredentials;
+        }
+
+        /// Check if currently authenticated. Will throw an exception of some sort which shoudl be caught by a general
+        /// exception handler and returned to the user as a 401, if not authenticated. Will return a fresh token if
+        /// the user is authenticated, which will reset the expiry.
+
+        [HttpGet]
+        [Authorize("Bearer")]
+        public dynamic Get()
+        {
+            /* 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            THIS METHOD SHOULD BE REMOVED IN PRODUCTION USE-CASES - IT ALLOWS A USER WITH 
+            A VALID TOKEN TO REMAIN LOGGED IN FOREVER, WITH NO WAY OF EVER EXPIRING THEIR
+            RIGHT TO USE THE APPLICATION.
+            Refresh Tokens (see https://auth0.com/docs/refresh-token) should be used to 
+            retrieve new tokens. 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            */
+            bool authenticated = false;
+            string user = null;
+            int entityId = -1;
+            string token = null;
+            DateTime? tokenExpires = default(DateTime?);
+
+            var currentUser = HttpContext.User;
+            if (currentUser != null)
+            {
+                authenticated = currentUser.Identity.IsAuthenticated;
+                if (authenticated)
+                {
+                    user = currentUser.Identity.Name;
+                    foreach (Claim c in currentUser.Claims) if (c.Type == "EntityID") entityId = Convert.ToInt32(c.Value);
+                    tokenExpires = DateTime.UtcNow.AddMinutes(2);
+                    token = GetToken(currentUser.Identity.Name, tokenExpires);
+                }
+            }
+            return new { authenticated = authenticated, user = user, entityId = entityId, token = token, tokenExpires = tokenExpires };
+        }
+
+        public class AuthRequest
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+        /// <summary>
+        /// Request a new token for a given username/password pair.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task <dynamic> Post([FromBody] AuthRequest req)
+        {
+            // Obviously, at this point you need to validate the username and password against whatever system you wish.
+          // if ((req.username == "TEST" && req.password == "TEST") || (req.username == "TEST2" && req.password == "TEST"))
+          /////following is ddedd (start)
+            var allowedOrigin = "*";
+            UserAccount appUser = null;
+            
+            appUser = await _accountService.FindUserAsync(req.username, req.password);
+
+            if (appUser == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+            /////following is ddedd (finish)
+            {
+                DateTime? expires = DateTime.UtcNow.AddMinutes(2);
+                var token = GetToken((req.username, expires);
+               // var token = GetToken(req.username, expires);
+                return new { authenticated = true, entityId = 1, token = token, tokenExpires = expires };
+            }
+            return new { authenticated = false };
+        }
+
+        private string GetToken(string user, DateTime? expires)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            // Here, you should create or look up an identity for the user which is being authenticated.
+            // For now, just creating a simple generic identity.
+            ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(user, "TokenAuth"), new[] { new Claim("EntityID", "1", ClaimValueTypes.Integer) });
+
+            var securityToken = handler.CreateToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                signingCredentials: tokenOptions.SigningCredentials,
+                subject: identity,
+                expires: expires
+                );
+            return handler.WriteToken(securityToken);
+        }
+    }
+}
---
+++ eTRIKS.Commons.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace eTRIKS.Commons.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //throw new Exception("Horsed");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [Authorize("Bearer")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
