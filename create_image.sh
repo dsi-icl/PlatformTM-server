1aa00f92e735cddaaab0bd60f4275739eaf33b5e
Ibrahim Emam <i.emam@imperial.ac.uk>
2018-03-16 22:58:13 +0000

    renaming

--- create_image.sh
+++ create_image.sh
@@ -3,7 +3,7 @@ set -x
 # Compile the project
 docker run -it --rm -v $(pwd):/sln --workdir /sln microsoft/aspnetcore-build:1.1-projectjson sh build.sh
 # Create the image
-docker build  -t platformtm/platformtm_server ./publish/web/
+docker build  -t platform-tm/platform-tm-server ./publish/web/
 
 # Create the db-init image
-docker build -t platformtm/db-init -f Dockerfile.db-init .
+docker build -t platform-tm/db-init -f Dockerfile.db-init .
