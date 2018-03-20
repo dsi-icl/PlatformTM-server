7ae584192b84866e00e8bd532dce1bcb36bd4065
Ibrahim Emam <i.emam@imperial.ac.uk>
2018-03-20 00:36:39 +0000

    removing old docker files

--- create_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-# Compile the project
-docker run -it --rm -v $(pwd):/sln --workdir /sln microsoft/aspnetcore-build:1.1-projectjson sh build.sh
-# Create the image
-docker build  -t platform-tm/platform-tm-server ./publish/web/
-
-# Create the db-init image
-docker build -t platform-tm/db-init -f Dockerfile.db-init .
